c763b31222c38d9339f4d9c4ebb7db26a960843c
Márk Győző Budai <mark.budai@adnovum.hu>
2023-03-24 09:19:08 +0100

    Gradle task, executed from Actions

--- buildSrc/src/main/groovy/com/example/github/GithubSecretsManager.groovy
+++ buildSrc/src/main/groovy/com/example/github/GithubSecretsManager.groovy
@@ -1,12 +1,21 @@
 package com.example.github
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskAction
 
 class GithubSecretsManager implements Plugin<Project> {
 
 	@Override
 	void apply(Project project) {
-		println "Hello from ${this.class.name}!"
+		project.tasks.register('githubSecretsManagerTask', GithubSecretsManagerTask)
+	}
+}
+
+abstract class GithubSecretsManagerTask extends DefaultTask {
+	@TaskAction
+	def action() {
+		println "Hello from task action"
 	}
 }
