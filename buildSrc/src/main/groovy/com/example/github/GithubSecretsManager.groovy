f1df3f1f374003f63b040a05b79515fe922900eb
Márk Győző Budai <mark.budai@adnovum.hu>
2023-03-24 09:38:31 +0100

    Look for github token

--- buildSrc/src/main/groovy/com/example/github/GithubSecretsManager.groovy
+++ buildSrc/src/main/groovy/com/example/github/GithubSecretsManager.groovy
@@ -1,5 +1,6 @@
 package com.example.github
 
+import org.apache.commons.lang3.StringUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -16,6 +17,14 @@ class GithubSecretsManager implements Plugin<Project> {
 abstract class GithubSecretsManagerTask extends DefaultTask {
 	@TaskAction
 	def action() {
+		def githubToken = System.getenv("GITHUB_TOKEN ")
+		if (StringUtils.isEmpty(githubToken)) {
+			println "NO github token found :("
+		} else {
+			println "GITHUB_TOKEN -> $githubToken"
+		}
+
+
 		System.getenv().forEach { key, value ->
 			println "$key -> $value"
 		}
